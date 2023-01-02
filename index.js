d146964e48427de115a6d975cf63b5d4e9bae77c
Hayden Nault <naulth@gmail.com>
2023-01-02 13:32:36 -0500

    pushing up code

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+const employee = {
+    name: "Hayden",
+    streetAddress: "Dunmore Pl"
+};
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+
+    const updatedEmployee = { ...employee };
+  
+    updatedEmployee[key] = value;
+  
+    console.log(updatedEmployee);
+    return updatedEmployee;
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
+
+    employee[key] = value;
+    return employee
+   
+  }
+
+  function deleteFromEmployeeByKey(employee, key, value) {
+
+    const employeeWithKeyRemoved = { ...employee };
+  
+   delete employeeWithKeyRemoved[key]
+
+    return employeeWithKeyRemoved;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey (employee, key, value) {
+
+    delete employee[key];
+    return employee
+  }
